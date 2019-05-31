cffbb1bbc4ecaf234c8e71447251e3d4f6015f8b
dorababu5757 <41118949+dorababu5757@users.noreply.github.com>
2019-05-31 19:48:55 +0530

    Update MainTaskAdapter.java

--- src/main/java/com/camunda/demo/springboot/adapter/MainTaskAdapter.java
+++ src/main/java/com/camunda/demo/springboot/adapter/MainTaskAdapter.java
@@ -16,7 +16,7 @@ public class MainTaskAdapter implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		System.out.println("***********All tasks are completed**************");
-		emailServiceImpl.sendSimpleMessage("pakkurthi.dorababu@1rivet.com", "Task Completed",
+		emailServiceImpl.sendSimpleMessage("test@test.com", "Task Completed",
 				execution.getVariable("result").toString()+" "+execution.getVariable("finalstatus"));
 
 	}
